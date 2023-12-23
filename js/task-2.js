0345c72af453a121d85c8593e4dceb9c7d25f4e2
nksenechkav <144259943+nksenechkav@users.noreply.github.com>
2023-12-23 17:25:39 +0100

    add fix in task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,6 @@
 class Storage {
     #items;
+
     constructor(items) {
         this.#items = items;
       }
@@ -7,9 +8,11 @@ class Storage {
     getItems() {
         return this.#items;
     }
+
     addItem(newItem) {
         this.#items.push(newItem);
     }
+    
     removeItem(itemToRemove) {
         const index = this.#items.indexOf(itemToRemove);
         if (index !== -1) {
